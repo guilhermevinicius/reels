7af6ac6482902bac71f55f616c0a9582fa19310e
Guilherme Vinicius Mendonca Ferreira <guilherme.mendonca@iatec.com>
2025-04-12 23:16:24 -0300

    chore: add configurations api

--- microservices/backoffice/src/Reels.Backoffice.Api/Configurations/ApiDependencyInjection.cs
+++ microservices/backoffice/src/Reels.Backoffice.Api/Configurations/ApiDependencyInjection.cs
@@ -1,4 +1,5 @@
 using Reels.Backoffice.Api.Extensions;
+using Scalar.AspNetCore;
 
 namespace Reels.Backoffice.Api.Configurations;
 
@@ -6,12 +7,41 @@ internal static class ApiDependencyInjection
 {
     internal static IServiceCollection ConfigureApi(this IServiceCollection services)
     {
+        services.AddCors();
+
+        services.AddOpenApi();
+
+        services.AddProblemDetails();
+
+        services.AddApiVersioning()
+            .AddApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+        services.AddControllers();
+
         return services;
     }
 
     internal static WebApplication UseApi(this WebApplication app)
     {
+        app.MapOpenApi();
+
         app.ApplyMigrations();
+
+        app.MapScalarApiReference();
+
+        app.UseCors();
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
 
         return app;
     }
